490f7c59c0522cd31002ba0301daf525436437a7
JiHwan Yoon <yjh091500@naver.com>
2023-04-15 13:24:51 +0000

    dev_output encoding type change

--- inference.py
+++ inference.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
 
     dev_output = pd.read_csv('./data/dev.csv')
     dev_output['preds'] = val_predict
-    dev_output.to_csv('./dev_output.csv', index=False)
+    dev_output.to_csv('./dev_output.csv', index=False, encoding='cp949')
