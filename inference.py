cce70bd61682fe0fce99967c061016c5c02d57de
afterthougt <afterthougt@gmail.com>
2023-04-20 07:42:24 +0000

    Add dev set post analysis

--- inference.py
+++ inference.py
@@ -43,8 +43,8 @@ if __name__ == '__main__':
         predictions = trainer.predict(model=model, datamodule=dataloader)
 
         # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
-        predictions = list(round(float(i), 1) for i in torch.cat(predictions))
-        predictions = [min(5, max(0, x)) for x in predictions] # 5.1과 같은 값들을 5로, 0보다 작은 값들은 0으로 바꿔준다. 
+        predictions = list(round(float(i), 3) for i in torch.cat(predictions))
+        predictions = [min(5., max(0., x)) for x in predictions] # 5.1과 같은 값들을 5로, 0보다 작은 값들은 0으로 바꿔준다. 
 
         # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
         output = pd.read_csv('./data/sample_submission.csv')
@@ -53,8 +53,8 @@ if __name__ == '__main__':
 
         # valid dataset 예측값을 확인하여 사후 분석 수행을 위한 dev_output.csv 뽑아내기 
         val_predict = trainer.predict(model=model, datamodule=val_dataloader)
-        val_predict = list(round(float(i), 1) for i in torch.cat(val_predict))
-        val_predict = [min(5, max(0, x)) for x in val_predict]
+        val_predict = list(round(float(i), 3) for i in torch.cat(val_predict))
+        val_predict = [min(5., max(0., x)) for x in val_predict]
 
         dev_output = pd.read_csv('./data/dev.csv')
         dev_output['preds'] = val_predict
@@ -74,10 +74,10 @@ if __name__ == '__main__':
             predictions = trainer.predict(model=model, datamodule=dataloader)
 
             # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
-            predictions = list(round(float(i), 1) for i in torch.cat(predictions))
+            predictions = list(round(float(i), 3) for i in torch.cat(predictions))
             results.append(predictions)
         
-        predictions = np.round(np.mean(np.array(results), axis=0), 1)
+        predictions = np.round(np.mean(np.array(results), axis=0), 3)
 
         # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
         output = pd.read_csv('./data/sample_submission.csv')
@@ -87,5 +87,15 @@ if __name__ == '__main__':
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
         output.to_csv(f'./data/output/{run_name}.csv', index=False)
+        
+        # valid dataset 예측값을 확인하여 사후 분석 수행을 위한 dev_output.csv 뽑아내기 
+        val_predict = trainer.predict(model=model, datamodule=val_dataloader)
+        val_predict = list(round(float(i), 3) for i in torch.cat(val_predict))
+        val_predict = [min(5., max(0., x)) for x in val_predict]
+        
+        dev_output = pd.read_csv('./data/dev.csv')
+        dev_output['preds'] = val_predict
+        dev_output['diff'] = dev_output['label'] - dev_output['preds']
+        dev_output.to_csv(f'./outputs/dev_output_{run_name}.csv', index=False, encoding='cp949')
             
 
