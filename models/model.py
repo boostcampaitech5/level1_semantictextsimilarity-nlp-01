da0b1c69ca08e271081999fc2611312e6b2b0934
afterthougt <afterthougt@gmail.com>
2023-04-25 05:14:29 +0000

    Refactor model.py for better readability

--- models/model.py
+++ models/model.py
@@ -2,10 +2,11 @@ import torch
 import pytorch_lightning as pl
 from torchmetrics import PearsonCorrCoef, F1Score
 from transformers import AutoModelForSequenceClassification
+
 import wandb
 from wandb import AlertLevel
 from LR_scheduler import CosineAnnealingWarmupRestarts
-pl.seed_everything(420)
+
 
 class Model(pl.LightningModule):
     def __init__(self, model_name, lr, loss_function='L1Loss', bce=False):
@@ -17,18 +18,19 @@ class Model(pl.LightningModule):
         self.bce = bce
         self.loss_function = loss_function
 
-        # 사용할 모델을 호출합니다.
+        # 모델 호출
         self.plm = AutoModelForSequenceClassification.from_pretrained(
             pretrained_model_name_or_path=model_name, 
             num_labels=1,
         )
         
-        # loss와 evaluation metric을 정의합니다.
-        # self.loss_func = getattr(torch.nn, self.loss_function)()
+        # 손실 함수(loss function) 정의
         if self.loss_function == 'SmoothL1Loss':
             self.loss_func = getattr(torch.nn, self.loss_function)(beta=0.1)
         else: # L1Loss, MSELoss, etc.
             self.loss_func = getattr(torch.nn, self.loss_function)
+            
+        # 평가 지표(evaluation metric) 정의
         if self.bce:
             self.evaluation = F1Score(task='binary')
         else:
@@ -63,11 +65,9 @@ class Model(pl.LightningModule):
         pearson = self.evaluation(logits.squeeze(), y.squeeze())
         self.log('test_pearson', pearson)
 
-        wandb.alert(
-		        title='test_step',
-		        level=AlertLevel.INFO,
-		        text=f'test_pearson : {pearson}',
-		    )
+        wandb.alert(title='test_step',
+		            level=AlertLevel.INFO,
+		            text=f'test_pearson : {pearson}')
 
     def predict_step(self, batch, batch_idx):
         x = batch
@@ -77,17 +77,14 @@ class Model(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
         # https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup
-        # scheduler = CosineAnnealingWarmupRestarts(optimizer, first_cycle_steps=200, 
-        #                                           cycle_mult=1.0, max_lr=1e-5, min_lr=1e-6, 
-        #                                           warmup_steps=50, gamma=0.5) 
-        return [optimizer]
-    # {
-    #         "optimizer": optimizer,
-    #         "lr_scheduler": {
-    #             "scheduler": scheduler,
-    #             "interval": "step"  # 스텝별로 스케줄러를 업데이트하도록 설정
-    #         }
-    #     }
+        scheduler = CosineAnnealingWarmupRestarts(optimizer, first_cycle_steps=200, 
+                                                  cycle_mult=1.0, max_lr=1e-5, min_lr=1e-6, 
+                                                  warmup_steps=50, gamma=0.5) 
+        return {'optimizer': optimizer,
+                'lr_scheduler': {
+                    'scheduler': scheduler,
+                    'interval': 'step'
+                }}
 
     def on_train_epoch_start(self):
         # 에폭 시작 시 학습률을 로깅합니다.
