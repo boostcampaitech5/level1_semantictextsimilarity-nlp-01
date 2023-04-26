f83434f7553da386d88e653e3117fe3a54cb49c5
afterthougt <afterthougt@gmail.com>
2023-04-26 05:40:24 +0000

    style: Refactor args.py to comply with .flake8 conventions

--- args.py
+++ args.py
@@ -1,16 +1,16 @@
-import argparse
-from argparse import Namespace
 import json
+from argparse import Namespace
+
 
 def parse_arguments() -> Namespace:
     """config.json 파일의 내용을 argparse.Namespace 객체로 변환.
-    
+
     Returns:
         args (argparse.Namespace): config.json 파일의 내용을 포함하는 Namespace 객체.
     """
-    
+
     with open('./config.json', 'r') as f:
         config = json.load(f)
-        
+
     args = Namespace(**config)
     return args
